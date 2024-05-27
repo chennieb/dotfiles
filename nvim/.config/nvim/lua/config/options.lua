82dfb55487b8d60d14fa4f55718cc8a89b1dfcad
Chen Bai <chen.bai@basyskom.com>
2024-05-27 10:17:01 +0200

    change scrolloff

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -17,6 +17,7 @@ vim.opt.splitbelow = true           -- open new vertical split bottom
 vim.opt.splitright = true           -- open new horizontal splits right
 -- vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
 vim.opt.showmode = true             -- we are experienced, wo don't need the "-- INSERT --" mode hint
+vim.opt.scrolloff = 10
 
 -- Searching
 vim.opt.incsearch = true            -- search as characters are entered
