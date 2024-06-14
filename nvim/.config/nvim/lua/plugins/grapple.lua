ff65a097e0bc42e5c40b040805397cd99d7c4cf8
Chen Bai <chen.bai@basyskom.com>
2024-06-14 11:50:10 +0200

    change grapple keybinds

--- nvim/.config/nvim/lua/plugins/grapple.lua
+++ nvim/.config/nvim/lua/plugins/grapple.lua
@@ -9,7 +9,7 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     cmd = "Grapple",
     keys = {
-        { "m", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
+        { "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
         { "<C-T>", "<cmd>Grapple toggle_tags<cr>", desc = "Open tags window" },
         { "<C-N>", "<cmd>Grapple cycle backward<cr>", desc = "Go to previous tag" },
         { "<C-M>", "<cmd>Grapple cycle forward<cr>", desc = "Go to next tag" },
