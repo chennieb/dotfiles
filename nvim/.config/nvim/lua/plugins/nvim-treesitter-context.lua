ee9ffdf401c397c676fd332f68ec09a723293a45
Chen Bai <chen.bai@basyskom.com>
2024-06-14 11:52:02 +0200

    adding plugins

--- nvim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -1,3 +1,9 @@
 return {
-    "nvim-treesitter/nvim-treesitter-context"
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function ()
+        local context = require('treesitter-context')
+        context.setup{
+            max_lines = 1,
+        }
+    end
 }
