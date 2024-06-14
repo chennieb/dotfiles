f23e39942ff185e095908ed792bce3ded8604039
Chen Bai <chen.bai@basyskom.com>
2024-06-14 11:53:18 +0200

    change neogit to magic keybind

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -3,7 +3,14 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "sindrets/diffview.nvim",
-        "nvim-telescope/telescope.nvim"
     },
-    config = true
+    opts = {
+        mappings = {
+            popup = {
+                ["F"] = "PullPopup",
+                ["p"] = "PushPopup",
+                ["P"] = false,
+            }
+        }
+    }
 }
