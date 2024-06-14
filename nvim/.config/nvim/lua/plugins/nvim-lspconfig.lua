0fd7e273ac25748991c7e73c1cb5397ba9754d7d
Chen Bai <chen.bai@basyskom.com>
2024-06-14 11:51:05 +0200

    adding changed lsp conifg

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,11 @@
 return {
     "neovim/nvim-lspconfig",
     config = function ()
-        local keymap = vim.keymap -- for conciseness
+        local keymap = vim.keymap
         local default_ops = { noremap = true, silent = true }
 
-        keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, default_ops)
+        --keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, default_ops)
         keymap.set('n', '<F4>', function() vim.cmd('ClangdSwitchSourceHeader') end, default_ops)
-        keymap.set({'n', 'v'}, '<C-\'>', function() vim.lsp.buf.format() end, default_ops)
+        keymap.set({'n', 'v'}, '<M-\'>', function() vim.lsp.buf.format() end, default_ops)
     end,
 }
